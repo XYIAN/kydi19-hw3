bb16b0cf040283c93d446d12bc94cd2e30f25288
EC2 Default User <ec2-user@ip-172-31-31-17.us-east-2.compute.internal>
2019-10-16 21:38:26 +0000

    final update

--- js/functions.js
+++ js/functions.js
@@ -30,24 +30,44 @@ $(document).ready(function()
     
     $("#b1").on("click",function()
     {
-        alert("searchS button clicked " + $("#input").length);
+        //alert("searchS button clicked " + $("#input").length);
         // if($("#input1").length > 2)
         // {
+        
         var searchBar = $("#input1").val();
-        alert("searchBar val=" + searchBar);
+        //alert("searchBar val=" + searchBar);
+        var units = $("#region").val(); 
         $.ajax({
     
         method: "GET",
-        url:"https://api.openweathermap.org/data/2.5/weather?q="+searchBar+"&units=imperial&appid=876349ecdd8f244418c417cf7684cced",
+        url:"https://api.openweathermap.org/data/2.5/weather?q="+searchBar+"&units="+units+"&appid=876349ecdd8f244418c417cf7684cced",
         dataType: "json",
         success: function(data)
         {
             console.log(data); 
-            alert("please work:(");
+            //alert("please work:(");
             //alert(data.weather[0].main);
             $("#f1").html(data.main.temp);
-            $("#region").append("<option>" + data.main + "</option>");
-            $("#f2").html(data.weather.icon);
+            $("#f1").css("color", "red");
+            //$("#region").append("<option>" + data.main + "</option>");
+            //alert(data.weather.icon);
+            // $("#f2").append("<img src=" + data.weather[0].icon +">");
+            
+            $('#wicon').attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+            //"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            $("#f3").html(data.main.pressure);
+            $("#f3").css("color", "red");
+            $("#f4").html(data.main.humidity+"%");
+            $("#f4").css("color", "red");
+            $("#f5").html(data.wind.speed+"mph");
+            $("#f5").css("color", "red");
+            $("#f6").html(data.sys.sunset);
+            $("#f6").css("color", "red");
+            $("#f7").html(data.sys.sunrise);
+            $("#f7").css("color", "red");
+            $("#f8").html("Expect: " + data.weather[0].main);
+            $("#f8").css("color", "red");
+            
             
         }
         
